ed62332449e7f49628af9b47acadbc994d96fe23
Melisa-Zorraindo <melisazorraindo@gmail.com>
2025-08-15 08:47:25 +0200

    sets up project

---
+++ src/main/java/com/melisazor/historical_stock_prices/HistoricalStockPricesApplication.java
@@ -0,0 +1,13 @@
+package com.melisazor.historical_stock_prices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalStockPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricalStockPricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/melisazor/historical_stock_prices/HistoricalStockPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.melisazor.historical_stock_prices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricalStockPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
